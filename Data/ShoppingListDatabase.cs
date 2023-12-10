0bc35ef480fb63bb3f855c90a279089c2dea0368
RazvanChereghi <razvanchereghi@yahoo.com>
2023-12-10 12:49:28 +0200

    Lab 10

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -18,7 +18,26 @@ namespace Chereghi_Razvan_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
+
         }
+
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
+        }
+
         public Task<List<ShopList>> GetShopListsAsync()
         {
             return _database.Table<ShopList>().ToListAsync();
@@ -88,6 +107,11 @@ namespace Chereghi_Razvan_Lab7.Data
         public Task<int> DeleteListProductAsync(ListProduct listProduct)
         {
             return _database.DeleteAsync(listProduct);
+        }
+
+        public Task<int> DeleteShopAsync(Shop shop)
+        {
+            return _database.DeleteAsync(shop);
         }
     }
 }
