33a850cd532d9c6dc765b1280c7faac3243dd539
RazvanChereghi <razvanchereghi@yahoo.com>
2023-12-10 12:15:53 +0200

    lab9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -3,10 +3,10 @@ namespace Chereghi_Razvan_Lab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -19,5 +19,32 @@ public partial class ListPage : ContentPage
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext) 
+        { 
+            BindingContext = new Product() 
+        });
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        var selectedListProduct = listView.SelectedItem as ListProduct;
+
+        if (selectedListProduct != null)
+        {
+            await App.Database.DeleteListProductAsync(selectedListProduct);
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(selectedListProduct.ShopListID);
+        }
     }
 }
